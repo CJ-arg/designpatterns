07bdce709e241f84c99e63959a14c677e52fb2a4
unknown <janoncarlos@gmail.com>
2023-09-11 16:48:19 -0300

    observer

--- observer/observer.js
+++ observer/observer.js
@@ -2,16 +2,18 @@ class Subject {
   constructor() {
     this.observers = [];
   }
+
   subscribe(observer) {
-    this.observer.push(this.observer);
+    this.observers.push(observer);
   }
-  unSubscribe(observer) {
+
+  unsubscribe(observer) {
     this.observers = this.observers.filter((obs) => obs !== observer);
   }
 
   notify(data) {
-    this.observers.forEach((element) => {
-      element.refresh(data);
+    this.observers.forEach((e) => {
+      e.refresh(data);
     });
   }
 }
@@ -19,7 +21,24 @@ class Observer {
   constructor(fn) {
     this.fn = fn;
   }
+
   refresh(data) {
     this.fn(data);
   }
 }
+
+const s = new Subject();
+const o1 = new Observer((data) => console.log("observador UNO" + data));
+function change() {
+  s.notify(myText.value);
+}
+const o2 = new Observer((d) => {
+  div1.innerHTML = d;
+});
+const o3 = new Observer((d) => {
+  div2.innerHTML = d.split("").reverse().join("");
+});
+
+s.subscribe(o1);
+s.subscribe(o2);
+s.subscribe(o3);

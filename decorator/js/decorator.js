053e0b30294d40b658f5e76dedb77779dfa7e2bd
unknown <janoncarlos@gmail.com>
2023-09-12 16:34:31 -0300

    decorator

--- decorator/js/decorator.js
+++ decorator/js/decorator.js
@@ -1,30 +1,45 @@
+// component
 class ProductComponent {
   constructor(name) {
     this.name = name;
   }
+
   getDetail() {
     return `${this.name}`;
   }
 }
 
+// decorator
 class ProductDecorator {
   constructor(productComponent) {
     this.productComponent = productComponent;
   }
+
   getDetail() {
-    this.productComponent.getDetail();
+    return this.productComponent.getDetail();
   }
 }
 
-class ComercialInfoProductDecorator extends ProductDecorator {
+// decorator 1
+class CommercialInfoProductDecorator extends ProductDecorator {
   constructor(productComponent, tradename, brand) {
     super(productComponent);
+
     this.tradename = tradename;
     this.brand = brand;
   }
+
   getDetail() {
-    return `${this.tradename} ${this.brand}` + super.getDetail();
+    return `${this.tradename} ${this.brand} ` + super.getDetail();
   }
 }
 
 const productComponent = new ProductComponent("Cerveza");
+console.log(productComponent.getDetail());
+
+const commercialInfoProduct = new CommercialInfoProductDecorator(
+  productComponent,
+  "London Porter",
+  "Fuller's"
+);
+console.log(commercialInfoProduct.getDetail());

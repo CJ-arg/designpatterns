afd599a6f46f6d9ae14ce1f2b1c64b1239a39a69
unknown <janoncarlos@gmail.com>
2023-09-19 11:27:06 -0300

    builder

---
+++ builder/js/builder.js
@@ -0,0 +1,31 @@
+class Person {
+  constructor(name, lastName, age, country, city, hobbies) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.country = country;
+    this.city = city;
+    this.hobbies = hobbies;
+  }
+  getFullName() {
+    return this.name + " " + this.lastName;
+  }
+}
+class FormBuilder {
+  constructor() {
+    this.reset();
+  }
+  reset() {
+    this.name = "";
+    this.lastName = "";
+    this.age = 0;
+    this.country = "";
+    this.city = "";
+    this.hobbies = [];
+  }
+
+  setName(name) {
+    this.name = name;
+    return this;
+  }
+}
